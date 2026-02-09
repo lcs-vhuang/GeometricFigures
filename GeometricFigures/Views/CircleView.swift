55182696f34f38a5acb7347b5bf89debd9508f82
Venessa Huang <vhuang@lcs.on.ca>
2026-02-09 11:49:25 -0500

    Created groups for model and view layers of the app.

---
+++ GeometricFigures/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometricFigures
+//
+//  Created by 黃翊喬 on 2026/2/9.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
